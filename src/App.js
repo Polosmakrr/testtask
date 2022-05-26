5f7c493e9dc2c8f0057651496c5002f28434e12e
Polosmakr <polosmakrr@gmail.com>
2022-05-26 14:29:42 +0300

    1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { lazy, Suspense } from 'react';
 
 const Users = lazy(() => import('./components/Users/Users'));
 const SignUp = lazy(() => import('./components/SignUp/SignUp'));
-// const Succesfull = lazy(() => import('./components/Succesfull/Succesfull'));
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path="testtask" element={<Navigation />}>
           <Route path="users" element={<Users />} />
           <Route path="signup" element={<SignUp />} />
-          {/* <Route path="success" element={<Succesfull/>}/> */}
         </Route>
       </Routes>
     </Suspense>
